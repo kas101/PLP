f0bd4cd222725c89b059f4090cd35b44c7baddd5
kas101 <34551245+kas101@users.noreply.github.com>
2023-02-17 00:22:42 +0200

    Delete control_flow_assignment.dart

--- control_flow_assignment.dart
+++
@@ -1,9 +0,0 @@
-void main(List<String> args) {
-  print('Please enter a number: ');
-//   String? num = stdin.readLineSync();
-// var num2=(int) num;
-//   switch (num) {
-//     case ( num < 10):
-//       print('Your number is less than 10');
-//   }
-}
