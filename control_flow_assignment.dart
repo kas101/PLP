0cfaf31452e33d89de081b9752201fefdd025038
kas101 <kasjacob@outlook.com>
2023-02-25 22:11:38 +0200

    Dart Assignment on control flow complete

--- control_flow_assignment.dart
+++ control_flow_assignment.dart
@@ -1,9 +1,17 @@
+//DART ASSIGNMENT ON CONTROL FLOW
+
+import 'dart:io';
+
 void main(List<String> args) {
   print('Please enter a number: ');
-//   String? num = stdin.readLineSync();
-// var num2=(int) num;
-//   switch (num) {
-//     case ( num < 10):
-//       print('Your number is less than 10');
-//   }
+  String input = stdin.readLineSync().toString();
+  double number = double.parse(input);
+
+  if (number < 10) {
+    print('Your number is less than 10');
+  } else if (number == 10) {
+    print('Your number is equal to 10');
+  } else {
+    print('Your number is greater than 10');
+  }
 }
